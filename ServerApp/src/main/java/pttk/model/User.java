3046543769ff89c1da558daea82ec6b8f6926e25
Piotr <gogola.piotr@interia.pl>
2019-01-05 03:17:13 +0100

    naive merge

---
+++ ServerApp/src/main/java/pttk/model/User.java
@@ -0,0 +1,102 @@
+package pttk.model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
+
+@MappedSuperclass
+public abstract class User {
+    @Transient
+    private static final String INVALID_SURNAME_MSG = "Invalid surname";
+    @Transient
+    private static final String INVALID_NAME_MSG = "Invalid name";
+    @Transient
+    private static final String NAME_REGEX = "[a-zA-Z]+";
+
+
+    @Id
+    private String login;
+
+    @Column(name="pass")
+    private String password;
+
+    @Column
+    private String name;
+
+    @Column
+    private String surname;
+
+    @Column(unique = true, nullable = false)
+    private Email email;
+
+    @Column(unique = true, nullable = false)
+    private Pesel pesel;
+
+    public User(){}
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(isNameValid(name))
+            this.name = name;
+        else
+            throw new IllegalArgumentException(INVALID_NAME_MSG);
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        if(isNameValid(surname))
+            this.surname = surname;
+        else throw new IllegalArgumentException(INVALID_SURNAME_MSG);
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+
+    public void setEmail(String email) {
+        this.email = new Email(email);
+    }
+
+    public Pesel getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(Pesel pesel) {
+        this.pesel = pesel;
+    }
+
+
+    private boolean isNameValid(String name)
+    {
+        return name.matches(NAME_REGEX);
+    }
+
+
+}
